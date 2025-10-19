116ee484442d3c9d099281bd51c177ca623bb768
henriquefac <henriquefac@edu.unifor.br>
2025-10-19 12:54:38 -0300

    Adicionado denoise otimizado

--- src/process/audio_to_text_prototype/parts/remove_noise/__init__.py
+++ src/process/audio_to_text_prototype/parts/remove_noise/__init__.py
@@ -1,2 +1,3 @@
 from .clean_audio import denoise_audioIO
-__all__ = ["denoise_audioIO"]
+from .clean_aduio_optimized import denoise_audioIO_norm
+__all__ = ["denoise_audioIO", "denoise_audioIO_norm"]
---
+++ src/process/audio_to_text_prototype/parts/remove_noise/clean_aduio_optimized.py
@@ -0,0 +1,38 @@
+import subprocess
+import io
+from configPy import Config
+
+models_dir = Config.get_dir_noise_models()
+path_rnnoise = models_dir.get_any("sh.rnnn")
+
+
+def denoise_audioIO_norm(audio_buffer: io.BytesIO, sample_rate:int=16000) -> io.BytesIO:
+
+    af_filter = f"arnndn=m={str(path_rnnoise)},loudnorm=I=-20:LRA=7:tp=-1"
+
+    cmd = [
+        "ffmpeg", "-i", "pipe:0",
+        "-af", af_filter, 
+        "-f", "wav",
+        "-ar", str(sample_rate),
+        "pipe:1"
+    ]
+
+    proc = subprocess.Popen(
+        cmd,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+
+    data, err = proc.communicate(input=audio_buffer.read())
+
+    if proc.returncode != 0 or not data:
+        raise RuntimeError(f"Erro no denoise: {err.decode() if err else 'sem saída'}")
+
+    audio_buffer.seek(0)
+    audio_buffer.truncate(0)
+    audio_buffer.write(data)
+    audio_buffer.seek(0)
+
+    return audio_buffer
